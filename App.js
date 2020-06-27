f004b8b9ccceba21f4438d77b01bffe0c5545670
Einar David <einardavidvillarroel@gmail.com>
2020-06-26 23:18:31 -0400

    first commit

---
+++ App.js
@@ -0,0 +1,19 @@
+
+import React from 'react';
+
+
+
+import MainNavigator from './src/Vista/Navigator/MainNavigator'
+
+const App: () => React$Node = () => {
+  return (
+    <>
+
+      <MainNavigator />
+    </>
+  );
+};
+export default App;
+
+//<MenuScreen />
+
